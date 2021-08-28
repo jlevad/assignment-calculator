3ac953ff26adfe5dcb5b3894fb9b8bfd93ad84ee
Miftakhus Salam <smiftakhus@gmail.com>
2021-08-28 14:58:02 +0700

    <fix>(common): clear & AC button

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const insert = (event) => {
   let displayText = document.getElementById('calc');
-
   switch (event.target.innerHTML) {
     case '=':
       displayText.innerHTML = eval(displayText.innerHTML);
       break;
     case 'AC':
-      displayText.innerHTML = '';
+      displayText.innerHTML = '0';
       break;
     case 'CE':
       displayText.innerHTML = displayText.innerHTML.slice(0, -1);
+      if (displayText.innerHTML === '') {
+        displayText.innerHTML = '0';
+      } 
       break;
     default:
-      displayText.innerHTML += event.target.innerHTML;
+      if (displayText.innerHTML === '0') {
+        displayText.innerHTML = event.target.innerHTML;
+      } else {
+        displayText.innerHTML += event.target.innerHTML;
+      }
       break;
   }
 }
